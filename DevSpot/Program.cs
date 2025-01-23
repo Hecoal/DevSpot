6a1c8104abae21b99a9b28647aed9d4f899d237c
Hecoal <hectorcortez_2002@hotmail.com>
2025-01-23 13:00:58 -0600

    Added scaffolded login item

--- DevSpot/Program.cs
+++ DevSpot/Program.cs
@@ -1,4 +1,5 @@
 using DevSpot.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevSpot
@@ -13,6 +14,10 @@ namespace DevSpot
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
             });
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+            { 
+                options.SignIn.RequireConfirmedAccount = false;
+            }).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDBContext>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
